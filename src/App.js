2d901a5eaa7813c8bc1e92e0b8c51b40d54efade
snp1801 <snp1801@gmail.com>
2023-07-11 00:40:40 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+require('dotenv').config();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [search, setSearch] = useState('');
+    const [pass, setPass] = useState("");
+
+    // useEffect(() => {
+    //   return () => {
+    //     setIsMounted(false);
+    //   };
+    // }, []);
+
+    async function fetchData() {
+        const options = {
+            method: 'GET',
+            url: process.env.API_URL,
+            params: { q: `${search}` },
+            headers: {
+                'X-RapidAPI-Key': process.env.API_KEY,
+                'X-RapidAPI-Host': process.env.API_HOST
+            }
+        };
+
+        try {
+            const response = await axios.request(options);
+            setPass(response.data);
+            console.log(pass)
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    const changeSearch = (e) => {
+        e.preventDefault();
+        setSearch(e.target.value);
+    }
+
+
+    return (
+        <div className='fullApp'>
+            <div className='content'>
+                <h1>
+                    Weather App
+                </h1>
+                <div>
+                    <input className="cityInput" placeholder="Search a city" value={search} type="text" onChange={changeSearch} />
+                    <button onClick={fetchData}>Search</button>
+                    {pass === "" ? (
+                        <div>No Data</div>
+                    ) : (
+                        <div>
+                            <h3>Temperature : {pass.current.temp_c} 'C</h3>
+                            <h3>Location : {pass.location.name}</h3>
+                            <h3>Region : {pass.location.region}</h3>
+                            <h3>Country : {pass.location.country}</h3>
+                        </div>
+                    )}
+
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
